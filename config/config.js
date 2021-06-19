3a0a754c84cc6a7c26ad793d4401e2ce09520fe0
Ao <572220216@qq.com>
2021-06-20 00:09:38 +0200

    modify conf

---
+++ config/config.js
@@ -0,0 +1,7 @@
+// Copy this file as config.js in the same folder, with the proper database connection URI.
+
+module.exports = {
+    db: 'mongodb://127.0.0.1:27017/cmdlc',
+
+    db_dev: 'mongodb://127.0.0.1:27017/cmdlc',
+};
