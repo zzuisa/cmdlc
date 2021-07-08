06ddbf1e2d854207abfb4eb3166785143e479801
Ao <572220216@qq.com>
2021-07-08 02:14:07 +0200

    mongo

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 // Copy this file as config.js in the same folder, with the proper database connection URI.
 
 module.exports = {
-    db: 'mongodb://127.0.0.1:27017/cmdlc',
+    db: 'mongodb+srv://noders:4uL1ucBGBD1bUJ3I@cluster0.su6lf.mongodb.net/cmdlc?retryWrites=true&w=majority',
 
     db_dev: 'mongodb+srv://noders:4uL1ucBGBD1bUJ3I@cluster0.su6lf.mongodb.net/cmdlc?retryWrites=true&w=majority',
 };
